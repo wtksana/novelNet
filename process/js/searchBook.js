9cf2288f4c46ad3d97309284efe55a7f0e3e274f
wtksana <wtksana@gmail.com>
2017-05-15 18:51:28 +0800

    update

---
+++ process/js/searchBook.js
@@ -0,0 +1,32 @@
+$('#searchBtn').on('click', function () {
+    var text = $('#searchText').val()
+    searchBook(text)
+})
+
+var cheerio = require('cheerio')
+var superagent = require('superagent')
+var template = require('art-template')
+
+function searchBook(text) {
+    superagent.get('http://zhannei.baidu.com/cse/search?s=920895234054625192&q=' + encodeURI(text))
+        .end(function (err, sres) {
+            if (err) {
+                return next(err)
+            }
+            var $$ = cheerio.load(sres.text)
+            // var items = []
+            var result = $$('.result-game-item-detail');
+            var data = []
+            if (result.length == 0) {
+                console.log('null')
+            } else {
+                result.each(function (idx, element) {
+                    var title = $$(element).find('.result-game-item-title-link').attr('title')
+                    data.push(title)
+                    console.log(title)
+                })
+                var html = template(__dirname + '/process/template/list-group.art', data)
+                $('.list-group').html(html)
+            }
+        })
+}
